69ce064e0244b0786931e53c5e0fb37f220dacda
Callum Fraser <callum@projectcodex.co>
2017-05-08 14:19:25 +0200

    fixed filter problems

--- registration_number.js
+++ registration_number.js
@@ -3,28 +3,26 @@ var plateTxt = document.querySelector(".regNum");
 var regNumbers = document.querySelector(".regNumbers");
 var filterBtn = document.querySelector(".filterButton");
 var townFilter = document.getElementsByName("town");
-var townAmount = [];
 
 btn.addEventListener('click', function() {
-  if(plateTxt.value.length > 0){
+  if(0 < plateTxt.value.length && plateTxt.value.length < 11){
     var newPlate = document.createElement('div');
-    plateTxt.value.toUpperCase();
+    var capitalTxt = plateTxt.value.substr(0,2).toUpperCase() + plateTxt.value.substr(3);
     plateTxt.value.className = 'regPlates';
-    newPlate.textContent = plateTxt.value;
+    newPlate.textContent = capitalTxt;
     newPlate.className = 'regPlates';
     regNumbers.appendChild(newPlate);
     plateTxt.value = '';
-    townAmount.push(newPlate);
   }
 
   else{
-    alert('Please enter a plate value!')
+    alert('Please enter a valid plate value!')
   }
 
   });
 
 function filter(location){
-   for (var i=0; i<townAmount.length; i++){
+   for (var i=0; i<regNumbers.children.length; i++){
    if (regNumbers.children[i].textContent.startsWith(location)){
      regNumbers.children[i].style.display = 'block'
    }
@@ -36,16 +34,19 @@ function filter(location){
 
 filterBtn.addEventListener('click', function(){
 var printedPlates = document.querySelectorAll('.regPlates');
-for (var i=0; i<3; i++){
+for (var i=0; i<4; i++){
     if (townFilter[i].checked){
       switch(i){
-       case 0:
+        case 0:
+        filter('C')
+        break;
+       case 1:
        filter('CJ')
        break;
-       case 1:
-      filter('CA')
-       break;
        case 2:
+       filter('CA')
+       break;
+       case 3:
        filter('CY')
       }
 }
